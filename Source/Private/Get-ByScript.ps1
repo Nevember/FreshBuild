46588699710888a975b62c3022241eb46a3f4cd2
The Sharp Ninja <ninja@thesharp.ninja>
2020-10-09 14:45:24 -0500

    Gotta run scripts as commands.

--- Source/Private/Get-ByScript.ps1
+++ Source/Private/Get-ByScript.ps1
@@ -4,24 +4,19 @@ function Get-ByScript {
     )
     $script = $item.script;
     Write-Host ("{0}: Installing..." -f $item.name)
+    $argList = @("-c", $script)
+    if($item.parameters) {
+        $argList += $item.parameters;
+    }
     if($item.elevate) {
-        $argList = @("-c", $script)
-        if($item.parameters) {
-            $argList += $item.parameters;
-        }
         try{
             Start-Process -Wait -Verb RunAs -FilePath powershell -ArgumentList $argList
         } catch {
             Write-Error $_
         }
     } else {
-        $block = [Scriptblock]::Create($script)
-        $arglist = @();
-        if($item.parameters) {
-            $argList += $item.parameters;
-        }
         try{
-            Invoke-Command -ScriptBlock $block
+            Start-Process -Wait -NoNewWindow -FilePath powershell -ArgumentList $argList
         } catch {
             Write-Error $_
         }
