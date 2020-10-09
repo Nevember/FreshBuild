20ceac02cc76a25c0192fee6d327d77bd0c794d2
The Sharp Ninja <ninja@thesharp.ninja>
2020-10-09 14:19:00 -0500

    Arguments can be confusing in PS.

--- Source/Private/Get-ByScript.ps1
+++ Source/Private/Get-ByScript.ps1
@@ -5,9 +5,17 @@ function Get-ByScript {
     $script = $item.script;
     Write-Host ("{0}: Installing..." -f $item.name)
     if($item.elevate) {
-        Start-Process -Wait -Verb RunAs -FilePath powershell -ArgumentList "-c", $script, $item.parameters
+        $argList = @("-c", $script)
+        if($item.parameters) {
+            $argList += $item.parameters;
+        }
+        Start-Process -Wait -Verb RunAs -FilePath powershell -ArgumentList $argList
     } else {
         $block = [Scriptblock]::Create($script)
+        $arglist = @();
+        if($item.parameters) {
+            $argList += $item.parameters;
+        }
         Invoke-Command -ScriptBlock $block
     }
     Write-Host ("{0}: Installation complete." -f $item.name)
