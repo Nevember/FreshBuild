f09e70a36512bb521823d1c3a92f2a57cfaa5d49
The Sharp Ninja <ninja@thesharp.ninja>
2020-10-12 14:13:09 -0500

    Don't Elevate

--- Source/Private/Install-Chocolatey.ps1
+++ Source/Private/Install-Chocolatey.ps1
@@ -3,7 +3,7 @@ function Install-Chocolatey {
         "name": "Chocolatey",
         "source": "direct",
         "script": "Set-ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; iex ((New-Object System.Net.WebClient).DownloadString(\"https://chocolatey.org/install.ps1\"))",
-        "elevate": true }' | ConvertFrom-Json);
+        }' | ConvertFrom-Json);
 
     Get-ByScript $item;
 }
