0404e1b824bbe8adb4eeede02a62bec1640b3d53
The Sharp Ninja <ninja@thesharp.ninja>
2020-10-12 14:16:58 -0500

    I hate escaping quotes.

--- Source/Private/Install-Chocolatey.ps1
+++ Source/Private/Install-Chocolatey.ps1
@@ -2,7 +2,7 @@ function Install-Chocolatey {
     $item = ('{
         "name": "Chocolatey",
         "source": "direct",
-        "script": "Set-ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; iex ((New-Object System.Net.WebClient).DownloadString(\"https://chocolatey.org/install.ps1\"))",
+        "script": "Set-ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; iex ((New-Object System.Net.WebClient).DownloadString(\\\"https://chocolatey.org/install.ps1\\\"))",
         }' | ConvertFrom-Json);
 
     Get-ByScript $item;
