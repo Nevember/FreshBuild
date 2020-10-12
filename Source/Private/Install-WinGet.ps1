bd04017f4e06a413d217d8abbbebe233d79dfe67
The Sharp Ninja <ninja@thesharp.ninja>
2020-10-12 13:43:46 -0500

    elevate to install winget

--- Source/Private/Install-WinGet.ps1
+++ Source/Private/Install-WinGet.ps1
@@ -2,6 +2,7 @@ function Install-WinGet {
     $item = ('{
         "name": "WinGet",
         "source": "direct",
+        "elevate": true,
         "url": "https://github.com/microsoft/winget-cli/releases/download/v.0.2.2521-preview/Microsoft.DesktopAppInstaller_8wekyb3d8bbwe.appxbundle",
         "script": "& \"./downloads/Microsoft.DesktopAppInstaller_8wekyb3d8bbwe.appxbundle\"",
         "parameters": [
