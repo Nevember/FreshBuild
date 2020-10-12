60251111ccf7ee7a9fdcff06350d955cc79533d9
The Sharp Ninja <ninja@thesharp.ninja>
2020-10-12 14:01:53 -0500

    Yup.

--- Source/Private/Install-WinGet.ps1
+++ Source/Private/Install-WinGet.ps1
@@ -2,8 +2,8 @@ function Install-WinGet {
     $item = ('{
         "name": "WinGet",
         "source": "direct",
-        "url": "https://github.com/microsoft/winget-cli/releases/download/v.0.2.2521-preview/Microsoft.DesktopAppInstaller_8wekyb3d8bbwe.appxbundle",
-        "script": "& \"$env:USERPROFILE/.freshBuild/downloads/Microsoft.DesktopAppInstaller_8wekyb3d8bbwe.appxbundle\"",
+        "url": "",
+        "script": "invoke-webrequest https://github.com/microsoft/winget-cli/releases/download/v.0.2.2521-preview/Microsoft.DesktopAppInstaller_8wekyb3d8bbwe.appxbundle -outfile $env:USERPROFILE/.freshBuild/downloads/Microsoft.DesktopAppInstaller_8wekyb3d8bbwe.appxbundle; & $env:USERPROFILE/.freshBuild/downloads/Microsoft.DesktopAppInstaller_8wekyb3d8bbwe.appxbundle",
         "parameters": [
             "/quiet",
             "/passive",
