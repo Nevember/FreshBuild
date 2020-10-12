0f8084119a1de1ee80313e8fbb463ce15017880d
The Sharp Ninja <ninja@thesharp.ninja>
2020-10-12 13:58:48 -0500

    Fixed script path.

--- Source/Private/Install-WinGet.ps1
+++ Source/Private/Install-WinGet.ps1
@@ -3,7 +3,7 @@ function Install-WinGet {
         "name": "WinGet",
         "source": "direct",
         "url": "https://github.com/microsoft/winget-cli/releases/download/v.0.2.2521-preview/Microsoft.DesktopAppInstaller_8wekyb3d8bbwe.appxbundle",
-        "script": "& \"./downloads/Microsoft.DesktopAppInstaller_8wekyb3d8bbwe.appxbundle\"",
+        "script": "& \"$env:USERPROFILE/.freshBuild/downloads/Microsoft.DesktopAppInstaller_8wekyb3d8bbwe.appxbundle\"",
         "parameters": [
             "/quiet",
             "/passive",
