cb85ee716c5c16733a0f15071e4a92b62e459019
The Sharp Ninja <ninja@thesharp.ninja>
2020-10-12 14:23:01 -0500

    Better way to install WinGet

--- Source/Private/Install-WinGet.ps1
+++ Source/Private/Install-WinGet.ps1
@@ -3,7 +3,7 @@ function Install-WinGet {
         "name": "WinGet",
         "source": "direct",
         "url": "",
-        "script": "invoke-webrequest https://github.com/microsoft/winget-cli/releases/download/v.0.2.2521-preview/Microsoft.DesktopAppInstaller_8wekyb3d8bbwe.appxbundle -outfile $env:USERPROFILE/.freshBuild/downloads/Microsoft.DesktopAppInstaller_8wekyb3d8bbwe.appxbundle; & $env:USERPROFILE/.freshBuild/downloads/Microsoft.DesktopAppInstaller_8wekyb3d8bbwe.appxbundle",
+        "script": "invoke-webrequest https://github.com/microsoft/winget-cli/releases/download/v.0.2.2521-preview/Microsoft.DesktopAppInstaller_8wekyb3d8bbwe.appxbundle -outfile $env:USERPROFILE/.freshBuild/downloads/Microsoft.DesktopAppInstaller_8wekyb3d8bbwe.appxbundle; Add-AppxPackage $env:USERPROFILE/.freshBuild/downloads/Microsoft.DesktopAppInstaller_8wekyb3d8bbwe.appxbundle -InstallAllResources",
         "parameters": [
             "/quiet",
             "/passive",
