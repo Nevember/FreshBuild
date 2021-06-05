9909781981eebaf5fb4b097157c0b7b7c0a986cd
Spiral Chaotic <25832466+Nevember@users.noreply.github.com>
2021-06-05 13:19:57 -0400

    Use "latest" url to get installer
    Hi!
    
    Rather than specifically downloading v.0.2.2521-preview of the WinGet AppX installer, changing the URL slightly allows us to download the latest installer instead.

--- Source/Private/Install-WinGet.ps1
+++ Source/Private/Install-WinGet.ps1
@@ -3,7 +3,7 @@ function Install-WinGet {
         "name": "WinGet",
         "source": "direct",
         "url": "",
-        "script": "Add-AppxPackage https://github.com/microsoft/winget-cli/releases/download/v.0.2.2521-preview/Microsoft.DesktopAppInstaller_8wekyb3d8bbwe.appxbundle -InstallAllResources"
+        "script": "Add-AppxPackage https://github.com/microsoft/winget-cli/releases/latest/download/Microsoft.DesktopAppInstaller_8wekyb3d8bbwe.appxbundle -InstallAllResources"
     }' | ConvertFrom-Json);
 
     Get-ByScript $item;
